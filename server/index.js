2ea259adc6db5b3af1d405cff93cdbb1c94abe7f
Ver_s <aleksandra.bystrova@alatoo.edu.kg>
2026-05-22 12:33:23 +0600

    comit

--- server/index.js
+++ server/index.js
@@ -6,15 +6,15 @@ const PORT = 5000; // Исправили регистр переменной
 const app = express();
 
 // Создаем подключение к PostgreSQL, забирая строку соединения из конфига
-const client = new Client({
-  connectionString: config.get("dbUrl"), 
-});
+// const client = new Client({
+//   connectionString: config.get("dbUrl"), 
+// });
 
 const start = async () => {
   try {
     // Подключаемся к базе данных PostgreSQL
-    await client.connect();
-    console.log("Успешное подключение к PostgreSQL");
+    // await client.connect();
+    // console.log("Успешное подключение к PostgreSQL");
 
     app.listen(PORT, () => {
       console.log("Сервер запустился на порту:", PORT);
